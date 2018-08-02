e8bb753da099dd28c439b45ce76ce2ac36332071
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-02 15:58:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,16 +10,10 @@ end
 
 def sort_array_char_count(array)
   array.sort do |a,b|
-    if a.length == b.length
-    0
-    elsif a.length < b.length
-    -1
-    elsif a.length > b.length
-    1
-    end
+    a.length<=>b.length
   end
 end
 
 def swap_elements(array)
-    array[1], array[2] = array [2], array[1] 
-end 
+    array[1], array[2] = array [2], array[1]
+end
